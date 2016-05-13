bc804545d35c05015d02474f08086cae2df1cebb
Bob Colner <bobcolner@AirBob.local>
2016-05-13 00:19:17 -0700

    cleanup

--- s3rap/s3rap.py
+++ s3rap/s3rap.py
@@ -7,56 +7,35 @@ s3resource = boto3.resource('s3')
 s3client = boto3.client('s3')
 logger = logging.getLogger(__name__)
 
-def delete_object(bucket, key):
-    "Delete an object"
-    s3resource.Object(bucket, key).delete()
-    logger.debug('deleted object: {0}'.format(key))
-
 def get_object(bucket, key):
-    "Download an object into memory"
+    "Download S3 object into memory"
     data = s3resource.Object(bucket, key).get()["Body"].read()
     logger.debug('downlated object: {0}'.format(key))
     return data
 
-def copy_object(src_bucket, src_key, dst_bucket, dst_key):
-    "Copy an object to a new S3 location"
-    src_path = os.path.join(src_bucket, src_key)
-    s3resource.Object(dst_bucket, dst_key).copy_from(CopySource=src_path)
-    logger.debug('copying object from: {0} to:{1}'.format(src_key, dst_key))
-
-def exists_object(bucket, key):
-    "Check if an object exists in S3"
-    results = s3client.list_objects(Bucket=bucket, Prefix=key)
-    return 'Contents' in results
-
 def download_object(bucket, key, file_name=None):
-    "Download object to local file"
+    "Download S3 object to local file"
     if file_name is None:
         file_name = key.split('/')[-1]
     s3resource.Bucket(bucket).download_file(Key=key, Filename=file_name)
     logger.debug('downloaded object: {0}'.format(key))
 
 def get_gzip_object(bucket, key):
-    "Download and un-gzip object into memory"
-    datagzip = get_object(bucket, key)
-    data_b = gzip.decompress(data_gzip)
-    logger.debug('downloaded gziped object: {0}'.format(key))
-    return data_b.decode("utf-8")
+    "Download and un-gzip S3 object into memory"
+    data_gzip = get_object(bucket, key)
+    return gzip.decompress(data_gzip).decode("utf-8")
 
 def download_gzip_object(bucket, key):
-    "Download and un-gzip object to local file"
-    file_path = key.split('/')[-1]
-    download_object(bucket, key, file_path)
-    out_file = un_gzip(file_path)
-    logger.debug('downloaded gziped object: {0}'.format(key))
-    return out_file
+    "Download and un-gzip S3 object to local file"
+    download_object(bucket, key)
+    return un_gzip(file_path)
 
 def un_gzip(file_path):
     "Un-gzip local file"
     if file_path.endswith('.gz'):
         out_file = file_path.replace('.gz', '')
     else:
-        out_file = file_path + '.un-gziped'
+        out_file = file_path + '.un-gz'
     with gzip.open(file_path, 'rb') as gzip_f, open(out_file, 'wb') as out_f:
         data_b = gzip_f.read()
         out_f.write(data_b)
@@ -80,20 +59,21 @@ def upload_file(bucket, key, local_file):
         logger.debug('uploaded object: {0}'.format(key))
         os.remove(gzip_file)
 
+def copy_object(src_bucket, src_key, dst_bucket, dst_key):
+    "Copy an object to a new S3 location"
+    src_path = os.path.join(src_bucket, src_key)
+    s3resource.Object(dst_bucket, dst_key).copy_from(CopySource=src_path)
+    logger.debug('copying object from: {0} to:{1}'.format(src_key, dst_key))
+
+def exists_object(bucket, key):
+    "Check if an key exists in S3"
+    results = s3client.list_objects(Bucket=bucket, Prefix=key)
+    return 'Contents' in results
+
 def list_buckets():
     "List accessable buckets"
     buckets = s3client.list_buckets()
     return [ b['Name'] for b in buckets['Buckets'] ]
-
-def create_bucket(bucket):
-    "Create bucket"
-    s3resource.create_bucket(Bucket=bucket)
-    logger.debug('created bucket: {0}'.format(bucket))
-
-def delete_bucket(bucket):
-    "Delete bucket"
-    s3client.delete_bucket(Bucket=bucket)
-    logger.debug('deleted bucket: {0}'.format(bucket))
 
 def list_objects(bucket, prefix=''):
     "List objects in S3 bucket"
@@ -107,6 +87,23 @@ def list_objects(bucket, prefix=''):
         }
         results.append(obj_meta)
     return results
+
+def create_bucket(bucket):
+    "Create bucket"
+    s3resource.create_bucket(Bucket=bucket)
+    logger.debug('created bucket: {0}'.format(bucket))
+
+def delete_bucket(bucket):
+    "Delete bucket and all objects within"
+    for obj_meta in list_objects(bucket):
+        delete_object(bucket, key=obj_meta['key'])
+    s3client.delete_bucket(Bucket=bucket)
+    logger.debug('deleted bucket: {0}'.format(bucket))
+
+def delete_object(bucket, key):
+    "Delete an object"
+    s3resource.Object(bucket, key).delete()
+    logger.debug('deleted object: {0}'.format(key))
 
 def generate_presigned_url(bucket, key, exp_secs=100):
     "Genereate a tempory auth-signed URL to access an S3 object"
